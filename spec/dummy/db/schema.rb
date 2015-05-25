5fdfeb7d697b05010a069bdb95e88d9e314f7ec7
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-05-25 18:36:05 +0300

    Add index to headers parameters hstore column

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525132652) do
+ActiveRecord::Schema.define(version: 20150525153421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,7 @@ ActiveRecord::Schema.define(version: 20150525132652) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "headlines_headers", ["parameters"], name: "index_headlines_headers_on_parameters", using: :gin
 
 end
