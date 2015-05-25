496c5a5025473fc85eb06247d99cfb4da40cb5b4
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-05-25 18:55:11 +0300

    Add name attribute to security header

--- lib/headlines/security_headers/security_header.rb
+++ lib/headlines/security_headers/security_header.rb
@@ -1,9 +1,10 @@
 module Headlines
   module SecurityHeaders
     class SecurityHeader
-      attr_reader :params
+      attr_reader :name, :params
 
       def initialize(header)
+        @name = header[0]
         @header = header[1]
         @params = { value: @header, enabled: true }
       end
