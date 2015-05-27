502c866ab2632d68f1518d63d3ade0d129cc091b
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2015-05-27 18:49:11 +0300

    Split data alexa spec into data_alexa and data_alexa/domain

--- spec/headlines/data_alexa_spec.rb
+++ spec/headlines/data_alexa_spec.rb
@@ -5,23 +5,17 @@ module Headlines
     end
 
     let(:domain) { "flipkart.com" }
-    let(:alexa_data) { open_fixture("alexa_data.xml") }
+    let(:alexa_data) { open_fixture("alexa_data.xml").read }
 
     subject(:data) { described_class.new(domain) }
 
     before do
-      stub_request(:get, build_url(domain)).and_return(body: alexa_data, status: 200)
+      stub_request(:get, build_url(domain)).and_return(
+        body: alexa_data,
+        status: 200
+      )
     end
 
-    its(:description) { is_expected.to eq "Flipkart is a leading destination for online shopping in India." }
-    its(:country_name) { is_expected.to eq "India" }
-    its(:country_code) { is_expected.to eq "IN" }
-
-    describe "#catecories" do
-      subject(:categories) { data.categories }
-
-      its(:count) { is_expected.to eq 2 }
-      its(:first) { is_expected.to be_kind_of(described_class::Category) }
-    end
+    its(:xml) { is_expected.to eq alexa_data }
   end
 end
