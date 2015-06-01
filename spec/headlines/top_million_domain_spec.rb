5c99b0f4641ef488e8d98a71706cbe2a59e6fabb
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2015-06-01 19:05:51 +0300

    Add an ability to read only first 10000 domains

--- spec/headlines/top_million_domain_spec.rb
+++ spec/headlines/top_million_domain_spec.rb
@@ -15,6 +15,22 @@ module Headlines
       it "yields multiple times block passed to" do
         expect { |block| top.fetch_in_batches(batch_size: 2, &block) }.to yield_control.twice
       end
+
+      context "when limit is passed" do
+        it "returns only limited number of rows" do
+          expect do |block|
+            top.fetch_in_batches(batch_size: 2, limit: 2, &block)
+          end.to yield_control.once
+        end
+      end
+
+      context "when limit is bigger then batch_size" do
+        it "returns number of rows set by limit option" do
+          top.fetch_in_batches(batch_size: 4, limit: 2) do |rows|
+            expect(rows.size).to eq 2
+          end
+        end
+      end
     end
   end
 end
