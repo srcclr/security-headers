37bd89cb8c9bb81ebfd0996e360fad472c001319
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2015-06-26 16:15:29 +0300

    Add rank_select to migration

--- db/migrate/20150623165523_rename_domain_id_to_domain_name_on_domains_categories.rb
+++ db/migrate/20150623165523_rename_domain_id_to_domain_name_on_domains_categories.rb
@@ -1,4 +1,11 @@
 class RenameDomainIdToDomainNameOnDomainsCategories < ActiveRecord::Migration
+  RANK_SELECT = <<-SQL
+    dense_rank() OVER (
+      PARTITION BY headlines_categories.industry_id
+      ORDER BY headlines_domains.rank
+    ) AS industry_rank
+  SQL
+
   def up
     drop_view :headlines_industry_ranked_domains
     remove_index :headlines_domains_categories, :domain_id
