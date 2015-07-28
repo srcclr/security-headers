23d86ffe9b8f09a8ef5d6ba8f61f0afe75ef7df9
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-07-28 17:10:26 +0300

    Adjust X-CONTENTTYPE-OPTIONS score calculation

--- spec/lib/headlines/security_headers/x_content_type_options_spec.rb
+++ spec/lib/headlines/security_headers/x_content_type_options_spec.rb
@@ -1,9 +1,9 @@
 module Headlines
   module SecurityHeaders
     describe XContentTypeOptions do
-      describe "#parse" do
-        let(:name) { "x-content-type-options" }
+      let(:name) { "x-content-type-options" }
 
+      describe "#parse" do
         subject(:params) { described_class.new(name, value).params }
 
         context "header with nosniff value" do
@@ -16,6 +16,22 @@ module Headlines
           let(:value) { "wrong value" }
 
           its([:enabled]) { is_expected.to be_falsey }
+        end
+      end
+
+      describe "#score" do
+        subject(:score) { described_class.new(name, value).score }
+
+        context "for header with nosniff value" do
+          let(:value) { "nosniff" }
+
+          it { is_expected.to eq 1 }
+        end
+
+        context "for header with wrong value" do
+          let(:value) { "wrong value" }
+
+          it { is_expected.to eq 0 }
         end
       end
     end
