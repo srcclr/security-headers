2c8e3e3246dd73ff3e5eb8d10ee5b8c85ac3e49e
Caleb Fenton <calebjfenton@gmail.com>
2015-09-24 11:15:10 -0700

    Fix http_protocol_rules tests

--- spec/lib/headlines/security_headers/content_security_policy_http_protocol_rules_spec.rb
+++ spec/lib/headlines/security_headers/content_security_policy_http_protocol_rules_spec.rb
@@ -17,44 +17,44 @@ module Headlines
         context "with http: source" do
           let(:value) { "style-src http:;" }
 
-          it { is_expected.to eq(-3) }
+          it { is_expected.to eq(-1) }
         end
 
         context "with explicit declaration" do
           let(:value) { "default-src 'self'; style-src http://example.com;" }
 
-          it { is_expected.to eq(1) }
+          it { is_expected.to eq(3) }
         end
 
         context "with domain without protocol declaration" do
           let(:value) { "default-src 'self'; style-src example.com;" }
 
-          it { is_expected.to eq(1) }
+          it { is_expected.to eq(3) }
         end
 
         context "with domain starts with *" do
           context "without any protocol declaration" do
             let(:value) { "default-src 'self'; style-src */example.com" }
 
-            it { is_expected.to eq(1) }
+            it { is_expected.to eq(3) }
           end
 
           context "with http protocol declaration" do
             let(:value) { "default-src 'self'; style-src http: */example.com" }
 
-            it { is_expected.to eq(1) }
+            it { is_expected.to eq(3) }
           end
 
           context "with https protocol declaration" do
             let(:value) { "default-src 'self'; style-src https: */example.com" }
 
-            it { is_expected.to eq(2) }
+            it { is_expected.to eq(4) }
           end
 
           context "with http and https protocol declaration" do
             let(:value) { "default-src 'self'; style-src http: https: */example.com" }
 
-            it { is_expected.to eq(1) }
+            it { is_expected.to eq(3) }
           end
         end
 
@@ -62,19 +62,19 @@ module Headlines
           context "with whitelist domains" do
             let(:value) { "default-src 'self' https://google.com;" }
 
-            it { is_expected.to eq(-2) }
+            it { is_expected.to eq(0) }
           end
 
           context "with subdomains" do
             let(:value) { "default-src '*'; script-src https://subdomain.example.com;" }
 
-            it { is_expected.to eq(-4) }
+            it { is_expected.to eq(-2) }
           end
 
           context "with unsecure domains" do
             let(:value) { "default-src 'self'; style-src https://unsecurehost.com;" }
 
-            it { is_expected.to eq(2) }
+            it { is_expected.to eq(4) }
           end
         end
       end
