2b0dd26458200885468512d17b5bc93918b612b6
RamilGilmanov <ramil.gilmanov@flatstack.com>
2016-01-12 11:30:53 +0300

    Limit threads, scan by small chunks

--- lib/headlines/scan_domains/runner.rb
+++ lib/headlines/scan_domains/runner.rb
@@ -3,7 +3,7 @@ require "concurrent"
 module Headlines
   module ScanDomains
     class Runner
-      DEFAULT_BATCH_SIZE = 50
+      DEFAULT_PROCESS_COUNT = 8
 
       attr_reader :total_count, :progressbar, :logger
       private :total_count, :progressbar, :logger
@@ -15,58 +15,34 @@ module Headlines
       end
 
       def call
-        Headlines::Domain.find_in_batches(batch_size: batch_size) do |domains|
-          scan_domains(domains)
+        progress_hash = Concurrent::Map.new
+        progress_hash[:progress] = 0
 
-          break if progressbar.progress >= total_count
-        end
-      end
-
-      private
-
-      def batch_size
-        @batch_size ||= [default_batch_size, total_count].min
-      end
-
-      def default_batch_size
-        SiteSetting.scan_proccess_count || DEFAULT_BATCH_SIZE
-      end
-
-      def scan_domains(domains)
-        results = domains.map do |domain|
-          [domain, Headlines::ScanDomains::Scanner.new(domain).async.scan!]
+        results = process_count.times.map do |n|
+          Headlines::ScanDomains::Scanner.new(batch_size * (n + 1), batch_size, progress_hash).async.scan!
         end
 
         while results.any?
-          results.delete_if do |domain_with_result|
-            result = domain_with_result[1]
-            if result.complete?
-              progressbar.increment
+          results.delete_if do |result|
+            logger.log_exception(result.reason) if result.rejected?
 
-              result.fulfilled? ? save_result(result.value) : logger.log_failure(*domain_with_result)
-            end
             !result.pending?
           end
-
-          sleep(1)
+          progressbar.progress = progress_hash[:progress]
+          sleep(5)
         end
       rescue StandardError => exception
         logger.log_exception(exception)
       end
 
-      def save_result(result)
-        domain = result.domain
+      private
 
-        if result.success?
-          domain.build_last_scan(scan_params(result).merge!(domain_id: domain.id, ssl_enabled: result.ssl_enabled))
-          domain.save!
-        end
-
-        logger.log_scan_result(domain, result)
+      def batch_size
+        @batch_size ||= total_count.fdiv(process_count).ceil
       end
 
-      def scan_params(result)
-        result[:params].slice(:headers, :results, :score, :http_score, :csp_score)
+      def process_count
+        @process_count ||= SiteSetting.scan_proccess_count || DEFAULT_PROCESS_COUNT
       end
     end
   end
--- lib/headlines/scan_domains/scanner.rb
+++ lib/headlines/scan_domains/scanner.rb
@@ -1,22 +1,52 @@
 require_relative './../../../app/interactors/headlines/parse_security_headers'
 require_relative './../../../app/interactors/headlines/generate_results'
 require_relative './../../../app/interactors/headlines/analyze_domain_headers'
+require_relative './../../../app/models/headlines/domain'
 
 module Headlines
   module ScanDomains
     class Scanner
       include Concurrent::Async
 
-      attr_reader :domain
-      private :domain
+      DEFAULT_CHUNK_SIZE = 100.freeze
 
-      def initialize(domain)
+      attr_reader :domain, :logger
+      private :domain, :logger
+
+      def initialize(offset, total, progress_hash)
         super()
-        @domain = domain
+        @offset = offset
+        @total = total
+        @progress_hash = progress_hash
+        @processed = 0
+        @logger = Headlines::ScanDomains::ResultsLogger.new
       end
 
       def scan!
-        AnalyzeDomainHeaders.call(domain: domain)
+        begin
+          Headlines::Domain.offset(@offset).limit(DEFAULT_CHUNK_SIZE).each do |domain|
+            result = AnalyzeDomainHeaders.call(domain: domain)
+            save_result(result)
+            @progress_hash[:progress] += 1
+          end
+
+          @processed += DEFAULT_CHUNK_SIZE
+        end while @processed < @total
+      end
+
+      def save_result(result)
+        domain = result.domain
+
+        if result.success?
+          domain.build_last_scan(scan_params(result).merge!(domain_id: domain.id, ssl_enabled: result.ssl_enabled))
+          domain.save!
+        end
+
+        logger.log_scan_result(domain, result)
+      end
+
+      def scan_params(result)
+        result[:params].slice(:headers, :results, :score, :http_score, :csp_score)
       end
     end
   end
