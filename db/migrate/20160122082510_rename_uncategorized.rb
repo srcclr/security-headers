7205a45a7b83b08195e4da3150170160e49dc1cf
RamilGilmanov <ramil.gilmanov@flatstack.com>
2016-01-22 11:56:19 +0300

    Rename Uncategorized to Miscellaneous

---
+++ db/migrate/20160122082510_rename_uncategorized.rb
@@ -0,0 +1,21 @@
+class RenameUncategorized < ActiveRecord::Migration
+  class Headlines::Category < ActiveRecord::Base; end
+
+  def up
+    category = Headlines::Category.find_by(title: "Uncategorized", category_id: 1)
+    category.update(
+      title: "Miscellaneous",
+      topic: "Top/Miscellaneous",
+      category_id: 1
+    )
+  end
+
+  def down
+    category = Headlines::Category.find_by(title: "Miscellaneous", category_id: 1)
+    category.update(
+      title: "Uncategorized",
+      topic: "Top/Uncategorized",
+      category_id: 1
+    )
+  end
+end
