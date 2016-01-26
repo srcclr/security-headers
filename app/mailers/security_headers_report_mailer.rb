be94a775e1b627740c6180ae9a89dbba871a5723
ArthurZaharov <arthur.zaharov@flatstack.com>
2016-01-26 13:06:15 +0300

    Add premailer gem

--- app/mailers/security_headers_report_mailer.rb
+++ app/mailers/security_headers_report_mailer.rb
@@ -3,18 +3,19 @@ require_dependency "email/message_builder"
 class SecurityHeadersReportMailer < ActionMailer::Base
   include Email::BuildEmailHelper
 
+  attr_reader :user, :scan_result
+  private :user, :scan_result
+
   def report(user, scan_result)
-    email_opts = {
-      template: "security_headers_report",
-      html_override: html(user, scan_result)
-    }
+    @user = user
+    @scan_result = scan_result
 
     build_email(user.email, email_opts)
   end
 
   private
 
-  def html(user, scan_result)
+  def html
     ActionView::Base.new("plugins/security-headers/app/views").render(
       template: 'email/security_headers_report',
       format: :html,
@@ -23,5 +24,16 @@ class SecurityHeadersReportMailer < ActionMailer::Base
         scan_result: scan_result
       }
     )
+  end
+
+  def premailed_html
+    Premailer.new(html.to_s, with_html_string: true).to_inline_css
+  end
+
+  def email_opts
+    {
+      template: "security_headers_report",
+      html_override: premailed_html
+    }
   end
 end
